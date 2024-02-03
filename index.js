c976c5e2aeeff12cd192a1a7b3f3333d873433f2
pawankumar10306 <pawankumar10306@outlook.com>
2024-02-03 14:40:41 +0530

    basic chat app

---
+++ index.js
@@ -0,0 +1,30 @@
+const { log } = require('console');
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`A user connected with ID ${socket.id}`);
+
+  socket.on('message', (data) => {
+    socket.broadcast.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
+});
